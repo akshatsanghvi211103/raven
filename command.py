e5e8d72e02c3617c2d6c39511e38ebdabe590644
akshat <akshat.sanghvi@students.iiit.ac.in>
2024-11-23 14:46:33 +0530

    small changes

--- command.py
+++ command.py
@@ -1,1 +1,5 @@
+# ssd_scratch vsr model
 python test.py data.modality=video data/dataset=lrs3 experiment_name=vsr_prelrs3vox2_large_ftlrs3_test model/visual_backbone=resnet_transformer_large model.pretrained_model_path=/ssd_scratch/cvit/akshat/vsr_prelrs3vox2_large_ftlrs3.pth
+
+# home vsr model
+python test.py data.modality=video data/dataset=lrs3 experiment_name=vsr_prelrs3vox2_large_ftlrs3_test model/visual_backbone=resnet_transformer_large model.pretrained_model_path=/home2/akshat/vsr_prelrs3vox2_large_ftlrs3.pth
